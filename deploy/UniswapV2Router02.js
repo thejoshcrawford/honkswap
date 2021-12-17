6e4e70c669399c1cc1d8fcf290fbf76f0c1ab212
Josh Crawford <josh@crawford.cc>
2021-12-17 13:35:53 -0800

    Updated all MIST to HONK

--- deploy/UniswapV2Router02.js
+++ deploy/UniswapV2Router02.js
@@ -1,4 +1,4 @@
-const { WNATIVE } = require("@mistswapdex/sdk");
+const { WNATIVE } = require("@honkswapdex/sdk");
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
   const { deploy } = deployments;
